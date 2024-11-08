080ba9ee7a5ff3e5f0134787e9dea48b53f02327
mfarinha <mikef4r@gmail.com>
2024-11-08 16:53:57 -0600

    Added more database endpoints

--- backend/server.py
+++ backend/server.py
@@ -4,7 +4,7 @@
 # Import flask and datetime module for showing date and time
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from database import user, members, inventory, get_db_session
+from database import user, members, inventory, discardedItems, reports, get_db_session
 # Initializing flask app
 app = Flask(__name__)
 CORS(app)
@@ -18,6 +18,38 @@ def get_inventory():
     session.close()
     return jsonify(inventory_list)
 
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    session = get_db_session()
+    users = session.query(user).all()
+    user_list = [{'id': user.id, 'username': user.username, 'role': user.role} for user in users]
+    session.close()
+    return jsonify(user_list)
+
+@app.route('/api/members', methods=['GET'])
+def get_members():
+    session = get_db_session()
+    member = session.query(members).all()
+    member_list = [{'firstname': member.firstname, 'lastname': member.lastname} for member in member]
+    session.close()
+    return jsonify(member_list)
+
+@app.route('/api/discardedItems', methods=['GET'])
+def get_members():
+    session = get_db_session()
+    items = session.query(discardedItems).all()
+    discardedItems_list = [{'sku': item.sku, 'quantity': item.quantity, 'reason': item.reaons, 'discarddate': item.discarddate} for item in items]
+    session.close()
+    return jsonify(discardedItems_list)
+
+@app.route('/api/reports', methods=['GET'])
+def get_members():
+    session = get_db_session()
+    reports = session.query(reports).all()
+    report_list = [{'numbersisits': report.numbervisits, 'newintakes': report.newintakes, 'HousesServed': report.householdserved, 'totalpounds': report.totalpounds,'discarded': report.discarded} for report in reports]
+    session.close()
+    return jsonify(report_list)
+
 @app.route('/api/inventory', methods=['POST'])
 def add_inventory():
     inventory_data = request.json
@@ -27,14 +59,6 @@ def add_inventory():
     session.commit()
     session.close()
     return jsonify({'message': 'Item added successfully!'}), 201
-
-@app.route('/api/users', methods=['GET'])
-def get_users():
-    session = get_db_session()
-    users = session.query(user).all()
-    user_list = [{'id': user.id, 'username': user.username, 'role': user.role} for user in users]
-    session.close()
-    return jsonify(user_list)
 
 @app.route('/api/users', methods=['POST'])
 def add_user():
@@ -46,14 +70,6 @@ def add_user():
     session.close()
     return jsonify({'message': 'User added successfully!'}), 201
 
-@app.route('/api/members', methods=['GET'])
-def get_members():
-    session = get_db_session()
-    member = session.query(members).all()
-    member_list = [{'firstname': member.firstname, 'lastname': member.lastname} for member in member]
-    session.close()
-    return jsonify(member_list)
-
 @app.route('/api/members', methods=['POST'])
 def add_members():
     member_data = request.json
@@ -64,7 +80,55 @@ def add_members():
     session.close()
     return jsonify({'message': 'member added successfully!'}), 201
 
-    
+@app.route('/api/members/<int:memberid>', methods=['PUT'])
+def update_member(memberid):
+    user_data = request.json
+    session = get_db_session()
+    member = session.query(members).filter(members.id == memberid).first()
+
+    if not member:
+        session.close()
+        return jsonify({'message': 'Member not found'}), 404
+
+    member.techid = user_data.get('techid', members.techid)
+    member.firstname = user_data.get('first name', members.firstname)
+    member.lastname = user_data.get('last name', members.lastname)
+    member.address = user_data.get('address', members.addres)
+    member.householdminors = user_data.get('minors', members.householdminors)
+    member.householdadults = user_data.get('adults', members.householdadults)
+    member.householdseniors = user_data.get('seniors', members.householdseniors)
+    session.commit()
+    session.close()
+    return jsonify({'message': 'User updated successfully'}), 200
+
+@app.route('/api/members/<int:memberid>', methods=['DELETE'])
+def delete_member(memberid):
+    session = get_db_session()
+    member = session.query(members).filter(members.id == memberid).first()
+
+    if not member:
+        session.close()
+        return jsonify({'message': 'Member not found'}), 404
+
+    session.delete(member)
+    session.commit()
+    session.close()
+    return jsonify({'message': 'User deleted successfully'}), 200 
+
+@app.route('/api/users/<int:userid>', methods=['DELETE'])
+def delete_user(userid):
+    session = get_db_session()
+    user = session.query(user).filter(user.id == userid).first()
+
+    if not user:
+        session.close()
+        return jsonify({'message': 'user not found'}), 404
+
+    session.delete(user)
+    session.commit()
+    session.close()
+    return jsonify({'message': 'User deleted successfully'}), 200 
+
 # Running app
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
